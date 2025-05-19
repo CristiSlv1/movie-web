b246625e57d5a654014d0c54e92e137aec92705a
CristiSlv1 <strugaricristian9@gmail.com>
2025-05-19 16:01:28 +0300

    fixing data-source

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -1,7 +1,4 @@
-// src/data-source.ts
-import "reflect-metadata";
 import { DataSource } from "typeorm";
-import { Genre } from "./entities/Genre";
 import { Movie } from "./entities/Movie";
 import { Review } from "./entities/Review";
 import { User } from "./entities/User";
@@ -9,15 +6,9 @@ import { Log } from "./entities/Log";
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "postgres",
-    password: "1606",
-    database: "movies_db",
+    url: process.env.DATABASE_URL,
+    ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
+    synchronize: true,
     logging: false,
-    entities: [Genre, Movie, Review, User, Log],
-    migrations: [],
-    subscribers: [],
-    synchronize: false, // Disable auto-sync
-
+    entities: [Movie, Review, User, Log],
 });
